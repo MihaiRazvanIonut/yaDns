d2bb11113da28a057cdb61efc385282baf88a840
MihaiRazvanIonut <razvan.mihai3781@gmail.com>
2024-01-18 07:51:07 +0200

    added master files to database

--- yaDns/protocol/dns_protocol.h
+++ yaDns/protocol/dns_protocol.h
@@ -2,7 +2,7 @@
  *  header containing structures used in the DNS protocol as described in RFC1034, RFC1035 
  *
 */
-
+#include <stdbool.h>
 #define MAX_LABEL_SIZE 64    // octets ( 63 chars + '\0')
 #define MAX_NAME_SIZE 256    // octets (255 chars + '\0')
 #define MAX_MESSAGE_SIZE 512 // octets 
@@ -47,18 +47,12 @@ typedef struct QuestionInfo {
 } QuestionInfo;
 
 typedef struct MessageHeader {
-    unsigned short id;
-    /**
-     * This member is responsible for a bunch of message flags
-     * QR 	    Query Response 1 bit: 0 for queries, 1 for responses.
-     * OPCODE 	Operation Code 4 bits: Typically always 0, see RFC1035 for details.
-     * AA 	    Authoritative Answer 1 bit: Set to 1 if the responding server is authoritative.
-     * TC 	    Truncated Message 1 bit: Set to 1 if the message length exceeds 512 bytes.
-     * RD 	    Recursion Desired 1 bit: Set by the sender of the request if the server should attempt to resolve the query recursively if it does not have an answer readily available.
-     * RA 	    Recursion Available 1 bit: Set by the server to indicate whether or not recursive queries are allowed.
-     * Z 	    Reserved 3 bits: Originally reserved for later use, but now used for DNSSEC queries.
-     * RCODE 	Response Code 4 bits: Set by the server to indicate the status of the response, i.e. whether or not it was successful or failed
-     */
+    bool qr;
+    bool aa;
+    bool tc;
+    bool rd;
+    bool ra;
+    unsigned short rcode;
     unsigned short queryInfo;
     unsigned short qdCount;
     unsigned short anCount;
